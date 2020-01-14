1f3c8ec1a8a0ec0a9cb6f639168814b43910ea5b
Anwesh43 <anweshthecool0@gmail.com>
2020-01-15 01:44:38 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbeakerbouncyfillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbeakerbouncyfillview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbeakerbouncyfillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.beakerbouncyfillview.BeakerBouncyFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BeakerBouncyFillView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
