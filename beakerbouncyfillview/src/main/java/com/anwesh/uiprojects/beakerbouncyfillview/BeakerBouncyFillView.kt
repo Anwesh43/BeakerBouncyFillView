29e8a551c4e4d45b3aa792f74de9e3fe4320f7f3
Anwesh43 <anweshthecool0@gmail.com>
2020-01-15 01:26:11 +0530

    adding Canvas extension methods

--- beakerbouncyfillview/src/main/java/com/anwesh/uiprojects/beakerbouncyfillview/BeakerBouncyFillView.kt
+++ beakerbouncyfillview/src/main/java/com/anwesh/uiprojects/beakerbouncyfillview/BeakerBouncyFillView.kt
@@ -11,6 +11,7 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.Canvas
+import android.graphics.RectF
 
 val nodes : Int = 5
 val lines : Int = 2
@@ -25,3 +26,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBeakerBouncyFill(scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sc1 : Float = sf.divideScale(0, 2)
+    val sc2 : Float = sf.divideScale(1, 2)
+    for (j in 0..(lines - 1)) {
+        save()
+        translate(-size * (1f - 2 * j), size)
+        rotate(-90f * (1f - 2 * j) * sc1.divideScale(j, lines))
+        drawLine(0f, 0f , 2 * size * (1f - 2 * j), 0f, paint)
+        restore()
+    }
+    save()
+    translate(-size, size)
+    drawRect(RectF(0f, -2 * size * sc2, 2 * size, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawBBFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBeakerBouncyFill(scale, size, paint)
+    restore()
+}
